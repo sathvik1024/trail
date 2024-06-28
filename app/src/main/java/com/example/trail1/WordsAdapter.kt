1f1f01e2bbf85c4bde6b8bab3583bab8726d341a
sathvik <sathvikgade@gmail.com>
2024-06-28 21:33:18 +0530

    recyclerivew part-3 -- impmlemented the suggestions

--- app/src/main/java/com/example/trail1/WordsAdapter.kt
+++ app/src/main/java/com/example/trail1/WordsAdapter.kt
@@ -1,6 +1,30 @@
 package com.example.trail1
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class WordsAdapter : RecyclerView.Adapter<WordsViewHolder> {
+class WordsAdapter : RecyclerView.Adapter<WordsAdapter.WordsViewHolder>() {
+
+
+    //avinash -- buy row planks from market n give it to sundar
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    //balaji --keeping count of no of items in the dataset n informs avinash
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    //chirag -- write the data on the row plank given by sundar
+    override fun onBindViewHolder(holder: WordsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    //sundar -- maintain the reserve planks in the holder box
+    class WordsViewHolder(itemView: View) : ViewHolder(itemView) {
+
+    }
 }
