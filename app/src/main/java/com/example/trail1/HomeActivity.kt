e3e6b15178ba1e1d3bc1d3dcdb67422747b9c8f9
sathvik <sathvikgade@gmail.com>
2024-07-05 17:31:55 +0530

    getting json

--- app/src/main/java/com/example/trail1/HomeActivity.kt
+++ app/src/main/java/com/example/trail1/HomeActivity.kt
@@ -11,58 +11,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.trail1.network.MarsApi
+import com.example.trail1.network.MarsApiService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
-    class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
-        AdapterView.OnItemClickListener {
-        var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
-        lateinit var mySpinner: Spinner
-        lateinit var myListview: ListView
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
-            setContentView(R.layout.activity_home)
-            mySpinner = findViewById(R.id.spinner) //taking handle
-            myListview = findViewById(R.id.listView)
-            myListview.isClickable = true
+    var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
 
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
 
-            mySpinner.onItemSelectedListener = this
-            myListview.setOnItemClickListener(this)
-            //get intent which sttarted this activity
-            //get the extras from that intent
-            //get the string with mykey
-            // var data = intent.extras?.getString("mykey")
-            /*  var data = intent.extras?.getString("mykey")
-              Log.i("homeactivity",data.toString())
-              //put the data either in a log or on the textview
-              var homeTextView:TextView = findViewById(R.id.tvHome)
-              homeTextView.setText(data)
-              homeTextView.setText(data)*/
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
 
-        override fun onItemSelected(
-            adpater: AdapterView<*>?,
-            view: View?,
-            position: Int,
-            id: Long
-        ) {
-            var item: String = adpater?.getItemAtPosition(position).toString()
-            Log.i(TAG, item)
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onItemClick(adpater: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            var item: String = adpater?.getItemAtPosition(position).toString()
-            Log.i(TAG, item)
-        }
     }
+
+    private fun getMarsPhotos() {
+        GlobalScope.launch {
+
+            var jsonString =   MarsApi.retrofitService.getPhotos()
+            Log.i("homeactivity",jsonString)
+        }
 }
+
+    fun getJson(view: View) {
+        getMarsPhotos()
+
+    }    }
